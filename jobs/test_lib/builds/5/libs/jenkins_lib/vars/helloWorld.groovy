3a68a541a1d1f90d2a70bcac60abfc1ecb35176a
Youssouy <traoreosman@yahoo.fr>
2020-04-12 04:28:11 +0000

    ajout des jobs jenkins

---
+++ jobs/test_lib/builds/5/libs/jenkins_lib/vars/helloWorld.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+import groovy.json.*
+def call (body) {
+    def mapVars            = [:]
+    body.resolveStrategy   = Closure.DELEGATE_FIRST
+    body.delegate          = mapVars
+    body()
+    
+    def name = mapVars.name
+    def action = mapVars.action
+    
+    println(action + " " + name + "!!")
+}
